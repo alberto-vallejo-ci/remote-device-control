43aa4a9b34eb7c1d149a4742139a9e2255ddf8f0
Alberto Vallejo <alberto.vallejo@crowdint.com>
2012-10-12 16:51:15 -0500

    Create Remote Device Control App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RDC::Application.config.secret_token = '0e3081089f1f0a45ded9e119c790cc9dc8ec30806bda93da517b1f62ccc84cfef92ea4aa90d4c40c784d2fc159bc138b616c55381aacf97267c8026d3754e7c4'
