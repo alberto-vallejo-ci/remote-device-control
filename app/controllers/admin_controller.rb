aa82e06f0a6f87fec44a4c308013a07e2fe630d5
Alberto Vallejo <alberto.vallejo@crowdint.com>
2012-10-12 22:38:06 -0500

    Add html basic authentication

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,14 @@
 class AdminController < ApplicationController
-  def index
+  before_filter :authenticate
 
+  def index
+  end
+
+  def authenticate
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |user, password|
+        user == ENV['USER'] && password == ENV['PASSWORD']
+      end
+    end
   end
 end
