40d24c78b2d4ce7f38f13ab348251f85ea7da412
Alberto Vallejo <alberto.vallejo@crowdint.com>
2012-10-14 11:49:14 -0500

    show device id in show client

--- app/controllers/client_connections_controller.rb
+++ app/controllers/client_connections_controller.rb
@@ -1,10 +1,16 @@
 class ClientConnectionsController < ApplicationController
+  respond_to :html
+
   def index
   end
+
   def create
      Pusher['devices'].trigger!('new_connection', { id: params[:id], name: "Device #{params[:id]}", device_class: 'on_hold'}) 
      redirect_to client_connection_path(params[:id])
   end
+
   def show
+    @device_id = params[:id]
+    respond_with @device_id
   end
 end
