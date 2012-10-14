70edf3e80cb50dd53e8a65836520bf3fab1c6eed
Alberto Vallejo <alberto.vallejo@crowdint.com>
2012-10-13 20:48:59 -0500

    Add a extra parameter to Pusher Trigger

--- app/controllers/client_connections_controller.rb
+++ app/controllers/client_connections_controller.rb
@@ -2,7 +2,7 @@ class ClientConnectionsController < ApplicationController
   def index
   end
   def create
-     Pusher['devices'].trigger!('new_connection', { id: params[:id], name: "Device #{params[:id]}"}) 
+     Pusher['devices'].trigger!('new_connection', { id: params[:id], name: "Device #{params[:id]}", device_class: 'on_hold'}) 
      redirect_to client_connection_path(params[:id])
   end
   def show
